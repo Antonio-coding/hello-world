3203e52d30dddf1012f5902489dbffdfbb13fa32
Antônio Thiago Neto <atn.coding@gmail.com>
2024-08-12 22:13:42 -0300

    Initial commit

---
+++ src/test/java/com/thiago_exercicio/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago_exercicio.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
